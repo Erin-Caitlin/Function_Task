062b732dbf89a3236172b79da6d088d68563a5df
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-23 09:34:50 +0200

    function

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// Question 1
+function greet() {
+    console.log("Hello");
+}
+greet()
+
+// Question 2
+function multiply(numb1, numb2) {
+    console.log(numb1 * numb2);
+}
+multiply(3, 6)
+
+// Question 3
+function tableOfFive(numb3) {
+    let result = numb3 * 5
+    console.log(result);
+    return result
+}
+tableOfFive(6)
